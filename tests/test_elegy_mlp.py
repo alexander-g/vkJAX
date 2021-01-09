bugfix broadcast_in_dim

tests/test_elegy_mlp.py
@@ -25,7 +25,7 @@ class MLP(elegy.Module):
             jax.nn.relu,
             elegy.nn.Linear(self.n2),
             jax.nn.relu,
-            elegy.nn.Linear(10),
+            elegy.nn.Linear(10, b_init=elegy.initializers.RandomNormal()),
         )
 
         return mlp(image)

vkjax/kompute_jaxpr_interpreter.py
@@ -166,7 +166,7 @@ class JaxprInterpreter:
             outbuf    = self.get_or_create_buffer(outvar)
             shape_in  = np.ones(len(outbuf.shape), dtype=int)
             for i,bcastdim in enumerate(equation.params['broadcast_dimensions']):
-                shape_in[bcastdim] = outbuf.shape[i]
+                shape_in[bcastdim] = inbuf.shape[i]
             shape_in  = ','.join(map(str, shape_in))
             shape_out = ','.join(map(str, outbuf.shape))
             n         = len(outbuf.shape)