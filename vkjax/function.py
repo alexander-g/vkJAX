c10b72339caafeed05da7a871856e4759efbc9cf
alexander-g <3867427+alexander-g@users.noreply.github.com>
2021-02-04 20:37:01 +0100

    restoring original output shapes

--- vkjax/function.py
+++ vkjax/function.py
@@ -1,6 +1,6 @@
 from .kompute_jaxpr_interpreter import JaxprInterpreter
 import numpy as np
-import jax
+import jax, jax.numpy as jnp
 
 import typing as tp
 
@@ -9,10 +9,13 @@ class Function:
         self.jaxpr_function = jax.make_jaxpr(function, static_argnums, return_shape=True)
         self.static_argnums = static_argnums
         self._jaxpr_interpreters = dict()
+        self._output_shapes      = dict()
     
     def __call__(self, *args:tp.Any, **kwargs:tp.Any) -> tp.Any:
-        jaxpr_interpreter = self._get_or_create_jaxpr_interpreter(args)
-        return jaxpr_interpreter.run(*args, **kwargs)
+        jaxpr_interpreter, output_shapes = self._get_or_create_jaxpr_interpreter(args)
+        output = jaxpr_interpreter.run(*args, **kwargs)
+        output = _restore_shapes(output, output_shapes)
+        return output
     
     def _get_or_create_jaxpr_interpreter(self, args:tp.Tuple[tp.Any]) -> JaxprInterpreter:
         leaves,structure = jax.tree_flatten(args)
@@ -22,4 +25,11 @@ class Function:
             #new input shapes or structure, need to re-trace
             jaxpr, output_shapes = self.jaxpr_function(*args)
             self._jaxpr_interpreters[shape_structure] = JaxprInterpreter(jaxpr, self.static_argnums)
-        return self._jaxpr_interpreters[shape_structure]
+            self._output_shapes[shape_structure]      = output_shapes
+        return self._jaxpr_interpreters[shape_structure], self._output_shapes[shape_structure]
+
+def _restore_shapes(x, targetshapes):
+    structure = jax.tree_structure(targetshapes)
+    x         = jax.tree_unflatten(structure, x)
+    x         = jax.tree_multimap(lambda a,shapestruct: jnp.asarray(a).reshape(shapestruct.shape), x, targetshapes )
+    return x
